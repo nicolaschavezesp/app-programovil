872522c499b5127547fefe16f207f3b3a5790ac9
nicolaschavezesp <107517916+nicolaschavezesp@users.noreply.github.com>
2023-11-12 23:43:48 -0300

    se le agrega lector qr directo desde home principal funcionando

--- src/app/homeprincipal/homeprincipal.module.ts
+++ src/app/homeprincipal/homeprincipal.module.ts
@@ -1,16 +1,15 @@
-// src/app/homeprincipal/homeprincipal.module.ts
-
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-
 import { HomeprincipalPage } from './homeprincipal.page';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    ZXingScannerModule,
     RouterModule.forChild([
       {
         path: '',
@@ -19,5 +18,6 @@ import { HomeprincipalPage } from './homeprincipal.page';
     ]),
   ],
   declarations: [HomeprincipalPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Agrega esta línea
 })
 export class HomeprincipalPageModule {}
--- src/app/homeprincipal/homeprincipal.page.ts
+++ src/app/homeprincipal/homeprincipal.page.ts
@@ -1,8 +1,7 @@
-// src/app/homeprincipal/homeprincipal.page.ts
-
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { BrowserQRCodeReader, DecodeHintType, NotFoundException, BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-homeprincipal',
@@ -10,32 +9,60 @@ import { Router } from '@angular/router';
   styleUrls: ['homeprincipal.page.scss'],
 })
 export class HomeprincipalPage {
+  // Agrega una referencia al componente zxing-scanner
+  @ViewChild('scanner') scanner: any;
+
+  // Declara la variable barcodeFormats aquí
+  barcodeFormats = [BarcodeFormat.QR_CODE];
+
   constructor(
     private menuCtrl: MenuController,
     private router: Router,
     private navCtrl: NavController
-    
   ) {}
 
   mostrarQRClases() {
     this.router.navigate(['/home']);
   }
 
-  lectorQR() {
-    // Lógica para leer el QR
+  async lectorQR() {
+    try {
+      const codeReader = new BrowserQRCodeReader();
+      const hints = new Map<DecodeHintType, any>();
+      hints.set(DecodeHintType.POSSIBLE_FORMATS, this.barcodeFormats);
+
+      // Utiliza la referencia al componente zxing-scanner para iniciar la cámara
+      await this.scanner.start();
+
+      // Actualiza la llamada al método decodeFromInputVideoDevice
+      const result = await codeReader.decodeFromInputVideoDevice(undefined, 'video');
+      this.handleDecodeResult(result);
+    } catch (error) {
+      console.error('Error al iniciar la cámara:', error);
+    }
+  }
+
+  // Función para manejar el resultado del escaneo
+  handleDecodeResult(result: any) {
+    if (result) {
+      console.log('Código QR leído:', result.getText());
+    } else {
+      console.log('No se detectó ningún código QR.');
+    }
+  }
+
+  // Agrega esta función para manejar el evento de escaneo
+  handleQRScan(event: any) {
+    // Maneja el escaneo según tus necesidades
+    console.log('Código QR escaneado:', event);
   }
 
   abrirMenu() {
     console.log('Abriendo menú...');
     this.menuCtrl.toggle();
   }
-  
 
   cerrarSesion() {
-    // Lógica para cerrar sesión
-    // Navegar a la página de login
     this.router.navigate(['/login']);
-    // Alternativamente, puedes usar navCtrl
-    // this.navCtrl.navigateForward('/login');
   }
 }
