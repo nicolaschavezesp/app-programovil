39300f30dea3d020262594ae56731064277c5c75
nicolaschavezesp <107517916+nicolaschavezesp@users.noreply.github.com>
2023-11-13 00:29:14 -0300

    se arregla vista de recuperar contraseña con alertas y mensajes respetando el diseño de la app

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { slideInAnimation } from '../Animations/slide-in.animation';
 
@@ -12,22 +12,42 @@ import { slideInAnimation } from '../Animations/slide-in.animation';
 export class RecuperarPage {
   usuario: string = ''; // Inicializa la propiedad usuario
 
+  constructor(
+    private navCtrl: NavController,
+    private router: Router,
+    private toastController: ToastController
+  ) {}
 
-  constructor(private navCtrl: NavController, private router: Router) {}
-
-
-  recuperarDatos() {
+  async recuperarDatos() {
     // Validar los campos
-    if (this.usuario.length >= 3 && this.usuario.length <= 8) {
-      // Los datos son válidos, pasar a la página Home
-      this.navCtrl.navigateForward('/login', {
-        queryParams: {
-          usuario: this.usuario 
-        },
-      });
+    if (this.isValidUser(this.usuario)) {
+      // Redirigir a la página de inicio de sesión
+      await this.presentToast('Revisa tu bandeja, te hemos enviado tu nueva contraseña.');
+      
     } else {
       // Mostrar un mensaje de error
-      // Puedes agregar lógica adicional para mostrar mensajes de error personalizados
+      await this.presentToast('Ingresa un usuario válido ');
     }
+  }
+  
+  // Función para verificar si el usuario es válido (tiene al menos dos caracteres y menos de 50)
+  private isValidUser(input: string): boolean {
+    const trimmedInput = input.trim();
+    return trimmedInput.length >= 2;
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000, // Duración del mensaje en milisegundos
+      position: 'bottom', // Posición del mensaje
+      color: 'primary', // Color del mensaje
+    });
+    toast.present();
+  }
+
+  
+  goBackLogin() {
+    this.navCtrl.navigateBack('/login');  // Navegar hacia atrás al login
   }
 }
